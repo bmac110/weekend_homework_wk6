210ffb5e793c8bdca47d8c49c0d750089874cda8
Billy Mackay <bossmack50@gmail.com>
2019-08-24 19:55:38 +0100

    save list is working

---
+++ js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript loaded");
+
+  const form = document.querySelector("#new-item-form");
+  form.addEventListener("submit", addNewGame);
+})
+
+const addEntryElement = function(text, element){
+  entry = document.createElement(element);
+  entry.textContent = text;
+  return entry
+}
+
+const addNewGame = function(event){
+  event.preventDefault();
+  console.log(this);
+  const title = this.title.value;
+  const developer = this.developer.value;
+  const genre = this.genre.value;
+
+  const gamingList = document.querySelector("#gaming-list");
+
+  titleEntry = addEntryElement(title, "h2");
+  developerEntry = addEntryElement(developer, "h3");
+  genreEntry = addEntryElement(genre, "h4");
+
+
+  const gameEntry = document.createElement("div")
+  gameEntry.textContent = ""
+  gameEntry.appendChild(titleEntry);
+  gameEntry.appendChild(developerEntry);
+  gameEntry.appendChild(genreEntry);
+  gamingList.appendChild(gameEntry);
+  gameEntry.classList.add("entry");
+}
